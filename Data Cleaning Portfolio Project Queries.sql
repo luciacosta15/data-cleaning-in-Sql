6ae444891aec4179408fcee72c279bfa70501092
Lúcia Costa <72652165+luciacosta15@users.noreply.github.com>
2021-09-19 12:20:26 +0100

    Update Data Cleaning Portfolio Project Queries.sql

--- Data Cleaning Portfolio Project Queries.sql
+++ Data Cleaning Portfolio Project Queries.sql
@@ -278,10 +278,6 @@ DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
 --USE PortfolioProject;
 --GO
 --SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
-
 
 
 
